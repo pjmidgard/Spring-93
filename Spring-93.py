0111872166bdcfce979859db06211718a610c649
Java Software <pj.midgard@yahoo.com>
2022-03-13 17:37:59 +0000

    Add files via upload

--- Spring-93.py
+++ Spring-93.py
@@ -246,7 +246,7 @@ class compression:
                                                             sda11="000"+sda11[1:2]+sda[3:]
 
                                                     if sda11[0:1]=="1" and former!=0 and sda11[3:4]=="0":
-                                                            sda11="001"+sda11[1:3]+sda[4:]
+                                                            sda11="01"+sda11[1:3]+sda[4:]
                                                     
 
                                                     if sda11[T10-1:T10]=="0":
