07fc0daa9fce164cc487cb52586e4db6e6cf5744
Java Software <pj.midgard@yahoo.com>
2022-03-13 15:09:58 +0000

    Add files via upload

--- Spring-93.py
+++ Spring-93.py
@@ -269,7 +269,7 @@ class compression:
                               
                                     
                                     lenfS=len(sda17)
-                                    print(lenfS)
+                                    #print(lenfS)
 
                                    
                                     Circle_times2=Circle_times2+1
